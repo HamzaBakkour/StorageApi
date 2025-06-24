fa28e781fab257d93a439c69a1152e5cac0a8573
HamzaBakkour <alexander.x4.hb@outlook.com>
2025-06-24 22:12:47 +0200

    Add project files.

---
+++ StorageApi/Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StorageApi.Models;
+
+namespace StorageApi.Data;
+
+
+public class ApplicationContext : DbContext
+{
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Product { get; set; } = default!;
+}
